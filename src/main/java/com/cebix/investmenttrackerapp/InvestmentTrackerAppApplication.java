4408b0b72b591f90d00a7bceec5c03d5b57f91f8
Cebix90 <cebulmat@gmail.com>
2024-04-30 22:05:48 +0200

    Initial commit

---
+++ src/main/java/com/cebix/investmenttrackerapp/InvestmentTrackerAppApplication.java
@@ -0,0 +1,13 @@
+package com.cebix.investmenttrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentTrackerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestmentTrackerAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cebix/investmenttrackerapp/InvestmentTrackerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cebix.investmenttrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentTrackerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
