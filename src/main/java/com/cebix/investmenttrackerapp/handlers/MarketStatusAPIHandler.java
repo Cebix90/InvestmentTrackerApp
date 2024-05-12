ecbf901179e16bbba86c4a061cac502952cf988b
Cebix90 <cebulmat@gmail.com>
2024-05-13 00:42:38 +0200

    Added MarketStatusAPIHandler and MarketStatusAPIHandlerTests classes

---
+++ src/main/java/com/cebix/investmenttrackerapp/handlers/MarketStatusAPIHandler.java
@@ -0,0 +1,38 @@
+package com.cebix.investmenttrackerapp.handlers;
+
+import com.cebix.investmenttrackerapp.constants.APIConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+@Component
+public class MarketStatusAPIHandler {
+    private final String MARKET_STATUS_URL = "https://api.polygon.io/v1/marketstatus/now";
+
+    private final WebClient webClient;
+
+    @Autowired
+    public MarketStatusAPIHandler(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(MARKET_STATUS_URL).build();
+    }
+
+    public Mono<String> getMarketStatusData() {
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(MARKET_STATUS_URL)
+                .queryParam("apiKey", APIConstants.API_KEY);
+
+        URI uri = builder.buildAndExpand().toUri();
+
+        return webClient.get()
+                .uri(uri)
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, response -> response.bodyToMono(String.class)
+                        .flatMap(errorResponse -> Mono.error(new RuntimeException("Error occurred with status code: " + response.statusCode() + " and message: " + errorResponse))))
+                .bodyToMono(String.class);
+    }
+}
