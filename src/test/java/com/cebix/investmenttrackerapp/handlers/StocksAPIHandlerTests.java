828468ab0915fcf4e04a3df072f364faeea23d28
Cebix90 <cebulmat@gmail.com>
2024-05-20 17:13:34 +0200

    [Update] APIHandlerTests moved to handlers package in test file

--- src/test/java/com/cebix/investmenttrackerapp/handlers/StocksAPIHandlerTests.java
+++ src/test/java/com/cebix/investmenttrackerapp/handlers/StocksAPIHandlerTests.java
@@ -1,12 +1,10 @@
-package com.cebix.investmenttrackerapp;
+package com.cebix.investmenttrackerapp.handlers;
 
-import com.cebix.investmenttrackerapp.handlers.StocksAPIHandler;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
