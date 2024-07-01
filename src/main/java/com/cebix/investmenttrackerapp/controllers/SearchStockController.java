093f92fb5a43f9bfa29cfca269b9cd0e4ddcd890
Cebix90 <cebulmat@gmail.com>
2024-07-01 17:17:00 +0200

    [Update] added functionality for getting last business day

--- src/main/java/com/cebix/investmenttrackerapp/controllers/SearchStockController.java
+++ src/main/java/com/cebix/investmenttrackerapp/controllers/SearchStockController.java
@@ -4,16 +4,20 @@ package com.cebix.investmenttrackerapp.controllers;
 import com.cebix.investmenttrackerapp.datamodel.Stock;
 import com.cebix.investmenttrackerapp.exceptions.FutureDateException;
 import com.cebix.investmenttrackerapp.exceptions.InvalidTickerException;
+import com.cebix.investmenttrackerapp.handlers.StocksAPIHandler;
 import com.cebix.investmenttrackerapp.services.StockService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.format.DateTimeParseException;
+
 @Controller
 public class SearchStockController {
 
-    private final StockService stockService = new StockService();
+    private final StocksAPIHandler stocksAPIHandler = new StocksAPIHandler();
+    private final StockService stockService = new StockService(stocksAPIHandler);
 
     @GetMapping("/searchStock")
     public String searchStockPage() {
@@ -27,7 +31,7 @@ public class SearchStockController {
         try {
             Stock stock = stockService.getStockData(ticker, date);
             model.addAttribute("stock", stock);
-        }  catch (InvalidTickerException | FutureDateException e) {
+        }  catch (InvalidTickerException | FutureDateException | DateTimeParseException e) {
             model.addAttribute("error", e.getMessage());
         } catch (Exception e) {
             model.addAttribute("error", "An unexpected error occurred. Please try again.");
--- src/main/java/com/cebix/investmenttrackerapp/services/StockService.java
+++ src/main/java/com/cebix/investmenttrackerapp/services/StockService.java
@@ -1,42 +1,46 @@
 package com.cebix.investmenttrackerapp.services;
 
 import com.cebix.investmenttrackerapp.datamodel.Stock;
-import com.cebix.investmenttrackerapp.exceptions.FutureDateException;
 import com.cebix.investmenttrackerapp.exceptions.InvalidTickerException;
 import com.cebix.investmenttrackerapp.handlers.StocksAPIHandler;
 import com.cebix.investmenttrackerapp.mappers.StockMapper;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeParseException;
 
 @Service
 public class StockService {
 
-    private final StocksAPIHandler stocksAPIHandler = new StocksAPIHandler();
+    private final StocksAPIHandler stocksAPIHandler;
+
+    @Autowired
+    public StockService(StocksAPIHandler stocksAPIHandler) {
+        this.stocksAPIHandler = stocksAPIHandler;
+    }
 
     public Stock getStockData(String ticker, String date) {
         if (ticker == null || ticker.isEmpty()) {
             throw new InvalidTickerException();
         }
 
+        LocalDate selectedDate;
         if (date == null || date.isEmpty()) {
-            date = LocalDate.now().minusDays(1).toString();
-        }
-
-        try {
-            LocalDate fromDate = LocalDate.parse(date);
-            LocalDate currentDate = LocalDate.now();
-
-            if (fromDate.isAfter(currentDate) || fromDate.equals(currentDate)) {
-                throw new FutureDateException();
+            selectedDate = getLastWorkingDay();
+        } else {
+            try {
+                selectedDate = LocalDate.parse(date);
+            } catch (DateTimeParseException e) {
+                throw new DateTimeParseException("Invalid date format: " + e.getParsedString(), e.getParsedString(), e.getErrorIndex());
             }
-        } catch (DateTimeParseException e) {
-            throw new DateTimeParseException("Invalid date format: " + e.getParsedString(), e.getParsedString(), e.getErrorIndex());
         }
 
-        String jsonData = stocksAPIHandler.getStockDataFromApi(ticker, "1", "day", date, date, 1).block();
+        String selectedDateString = selectedDate.toString();
+
+        String jsonData = stocksAPIHandler.getStockDataFromApi(ticker, "1", "day", selectedDateString, selectedDateString, 1).block();
 
         JSONObject jsonObject = new JSONObject(jsonData);
         int queryCount = jsonObject.getInt("queryCount");
@@ -47,5 +51,15 @@ public class StockService {
         }
 
         return StockMapper.mapJSONToStock(jsonData);
+    }
+
+    private LocalDate getLastWorkingDay() {
+        LocalDate date = LocalDate.now().minusDays(1);
+
+        while (date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY) {
+            date = date.minusDays(1);
+        }
+
+        return date;
     }
 }
