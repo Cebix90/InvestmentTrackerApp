311ac09a8194a1377ea1182dc22f961ea79406b6
Cebix90 <cebulmat@gmail.com>
2024-07-01 23:06:23 +0200

    [Updated] Added 2 last tests to StocksAPIHandlerTests which check cases when date is null or empty

--- src/test/java/com/cebix/investmenttrackerapp/services/StockServiceTests.java
+++ src/test/java/com/cebix/investmenttrackerapp/services/StockServiceTests.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 
@@ -19,6 +21,7 @@ import java.time.format.DateTimeParseException;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,57 +33,89 @@ public class StockServiceTests {
     @InjectMocks
     private StockService stockService;
 
-    @Test
-    public void testGetStockData_WithCorrectData_ReturnsCorrectStock() throws IOException {
-        String ticker = "AAPL";
-        String date = "2023-01-09";
-        String stockJSON = new String(Files.readAllBytes(Paths.get("src/test/resources/mappers/stock.json")));
+    private final String ticker = "AAPL";
+    private final String date = "2024-06-28";
 
-        when(stocksAPIHandler.getStockDataFromApi(anyString(), anyString(), anyString(), anyString(), anyString(), anyInt()))
+    @Test
+    public void testRetrieveStockData_WithCorrectData_ReturnsCorrectStock() throws IOException {
+        String stockJSON = new String(Files.readAllBytes(Paths.get("src/test/resources/services/validStock.json")));
+
+        when(stocksAPIHandler.getStockData(anyString(), anyString(), anyString(), anyString(), anyString(), anyInt()))
                 .thenReturn(Mono.just(stockJSON));
 
-        Stock stock = stockService.getStockData(ticker, date);
+        Stock stock = stockService.retrieveStockData(ticker, date);
 
         assertNotNull(stock);
         assertEquals(ticker, stock.getTicker());
-        assertEquals(130.15, stock.getValue());
+        assertEquals(210.62, stock.getValue());
         assertEquals(LocalDate.parse(date), stock.getDate());
     }
 
     @Test
-    public void testGetStockData_WithTickerAsNull_ThrowsInvalidTickerException() {
-        String date = "2023-01-09";
-
-        assertThrows(InvalidTickerException.class, () -> stockService.getStockData(null, date));
+    public void testRetrieveStockData_WithTickerAsNull_ThrowsInvalidTickerException() {
+        assertThrows(InvalidTickerException.class, () -> stockService.retrieveStockData(null, date));
     }
 
     @Test
-    public void testGetStockData_WithTickerAsEmptyString_ThrowsInvalidTickerException() {
-        String date = "2023-01-09";
-
-        assertThrows(InvalidTickerException.class, () -> stockService.getStockData("", date));
+    public void testRetrieveStockData_WithTickerAsEmptyString_ThrowsInvalidTickerException() {
+        assertThrows(InvalidTickerException.class, () -> stockService.retrieveStockData("", date));
     }
 
     @Test
-    public void testGetStockData_WithIncorrectDataFormat_ThrowsDateTimeParseException() {
-        String ticker = "AAPL";
-
-        assertThrows(DateTimeParseException.class, () -> stockService.getStockData(ticker, "09-01-2023"));
-        assertThrows(DateTimeParseException.class, () -> stockService.getStockData(ticker, "2023-1-9"));
-        assertThrows(DateTimeParseException.class, () -> stockService.getStockData(ticker, "1-9-2023"));
+    public void testRetrieveStockData_WithIncorrectDataFormat_ThrowsDateTimeParseException() {
+        assertThrows(DateTimeParseException.class, () -> stockService.retrieveStockData(ticker, "06-28-2024"));
+        assertThrows(DateTimeParseException.class, () -> stockService.retrieveStockData(ticker, "2024-6-28"));
+        assertThrows(DateTimeParseException.class, () -> stockService.retrieveStockData(ticker, "6-28-2024"));
     }
 
     @Test
-    public void testGetStockData_WithInvalidTicker_ThrowsInvalidTickerException() throws IOException {
+    public void testRetrieveStockData_WithInvalidTicker_ThrowsInvalidTickerException() throws IOException {
         String ticker = "asas";
-        String date = "2023-01-09";
-        String stockJSON = new String(Files.readAllBytes(Paths.get("src/test/resources/invalidTicker.json")));
+        String stockJSON = new String(Files.readAllBytes(Paths.get("src/test/resources/services/invalidTicker.json")));
 
-        when(stocksAPIHandler.getStockDataFromApi(anyString(), anyString(), anyString(), anyString(), anyString(), anyInt()))
+        when(stocksAPIHandler.getStockData(anyString(), anyString(), anyString(), anyString(), anyString(), anyInt()))
                 .thenReturn(Mono.just(stockJSON));
 
-        assertThrows(InvalidTickerException.class, () -> stockService.getStockData(ticker, date));
+        assertThrows(InvalidTickerException.class, () -> stockService.retrieveStockData(ticker, date));
     }
 
+    @Test
+    public void testRetrieveStockData_WithDateAsNull_ThrowsInvalidTickerException() throws IOException {
+        String stockJSON = new String(Files.readAllBytes(Paths.get("src/test/resources/services/validStock.json")));
 
+        when(stocksAPIHandler.getStockData(anyString(), anyString(), anyString(), anyString(), anyString(), anyInt()))
+                .thenReturn(Mono.just(stockJSON));
+
+        LocalDate mockToday = LocalDate.of(2024, 7, 1);
+        try (MockedStatic<LocalDate> mock = Mockito.mockStatic(LocalDate.class, CALLS_REAL_METHODS)) {
+            mock.when(LocalDate::now).thenReturn(mockToday);
+
+            Stock stock = stockService.retrieveStockData(ticker, null);
+
+            assertNotNull(stock);
+            assertEquals(ticker, stock.getTicker());
+            assertEquals(210.62, stock.getValue());
+            assertEquals(LocalDate.parse("2024-06-28"), stock.getDate());
+        }
+    }
+
+    @Test
+    public void testRetrieveStockData_WithDateAsEmptyString_ThrowsInvalidTickerException() throws IOException {
+        String stockJSON = new String(Files.readAllBytes(Paths.get("src/test/resources/services/validStock.json")));
+
+        when(stocksAPIHandler.getStockData(anyString(), anyString(), anyString(), anyString(), anyString(), anyInt()))
+                .thenReturn(Mono.just(stockJSON));
+
+        LocalDate mockToday = LocalDate.of(2024, 6, 30);
+        try (MockedStatic<LocalDate> mock = Mockito.mockStatic(LocalDate.class, CALLS_REAL_METHODS)) {
+            mock.when(LocalDate::now).thenReturn(mockToday);
+
+            Stock stock = stockService.retrieveStockData(ticker, "");
+
+            assertNotNull(stock);
+            assertEquals(ticker, stock.getTicker());
+            assertEquals(210.62, stock.getValue());
+            assertEquals(LocalDate.parse("2024-06-28"), stock.getDate());
+        }
+    }
 }
