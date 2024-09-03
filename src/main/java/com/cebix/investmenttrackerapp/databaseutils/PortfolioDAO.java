a1952ca01fb6554de077a0491578ed1d3f55a1e2
Cebix90 <cebulmat@gmail.com>
2024-09-03 23:13:00 +0200

    [Update] Added update methods for Portfolio entity

--- src/main/java/com/cebix/investmenttrackerapp/databaseutils/PortfolioDAO.java
+++ src/main/java/com/cebix/investmenttrackerapp/databaseutils/PortfolioDAO.java
@@ -1,5 +1,6 @@
 package com.cebix.investmenttrackerapp.databaseutils;
 
+import com.cebix.investmenttrackerapp.datamodel.CustomUser;
 import com.cebix.investmenttrackerapp.datamodel.Portfolio;
 import com.cebix.investmenttrackerapp.exceptions.UserNotFoundException;
 import jakarta.persistence.criteria.CriteriaBuilder;
@@ -8,6 +9,10 @@ import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.function.Consumer;
 
 public class PortfolioDAO {
     private final SessionFactory sessionFactory;
@@ -53,6 +58,55 @@ public class PortfolioDAO {
             } else {
                 throw new UserNotFoundException();
             }
+        }
+    }
+
+    public Portfolio updatePortfolioStocksCollection(Long customUserId, Map<String, Integer> newStocks) {
+        return updatePortfolioField(customUserId, portfolio -> portfolio.setStocks(newStocks), this::validateStock, newStocks);
+    }
+
+    public Portfolio updatePortfolioOverallValue(Long customUserId, double newOverallValue) {
+        return updatePortfolioField(customUserId, portfolio -> portfolio.setOverallValue(newOverallValue), this::validateOverallValue, newOverallValue);
+    }
+
+    public Portfolio updatePortfolioHistoricalValue(Long customUserId, Map<LocalDate, Double> newHistoricalValues) {
+        return updatePortfolioField(customUserId, portfolio -> portfolio.setHistoricalValue(newHistoricalValues), this::validateHistoricalValue, newHistoricalValues);
+    }
+
+    private <T> Portfolio updatePortfolioField(Long customUserId, Consumer<Portfolio> fieldUpdater, Consumer<T> validator, T value) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction transaction = session.beginTransaction();
+            Portfolio portfolioToUpdate = findPortfolioByUserId(customUserId);
+
+            if (portfolioToUpdate != null) {
+                validator.accept(value);
+                fieldUpdater.accept(portfolioToUpdate);
+                session.merge(portfolioToUpdate);
+                transaction.commit();
+                return portfolioToUpdate;
+            } else {
+                throw new UserNotFoundException();
+            }
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage(), e);
+        }
+    }
+
+    private void validateStock(Map<String, Integer> stocks) {
+        if (stocks == null || stocks.isEmpty()) {
+            throw new IllegalArgumentException("Stock must not be null or empty");
+        }
+    }
+
+    private void validateOverallValue(double overallValue) {
+        if (overallValue <= 0) {
+            throw new IllegalArgumentException("Overall value cannot be less than zero");
+        }
+    }
+
+    private void validateHistoricalValue(Map<LocalDate, Double> historicalValues) {
+        if (historicalValues == null || historicalValues.isEmpty()) {
+            throw new IllegalArgumentException("Historical Values must not be null or empty");
         }
     }
 }
