217cf647a28f119c9265ea3fbd55afd31229a250
Cebix90 <cebulmat@gmail.com>
2024-09-23 23:35:01 +0200

    [Added] Added SecurityController

---
+++ src/main/java/com/cebix/investmenttrackerapp/controllers/SecurityController.java
@@ -0,0 +1,30 @@
+package com.cebix.investmenttrackerapp.controllers;
+
+import com.cebix.investmenttrackerapp.databaseutils.CustomUserDAO;
+import com.cebix.investmenttrackerapp.databaseutils.CustomUserSessionFactory;
+import com.cebix.investmenttrackerapp.datamodel.CustomUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SecurityController {
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "security/login";
+    }
+
+    @GetMapping("/register")
+    public String getRegisterPage(Model model) {
+        model.addAttribute("customUser", new CustomUser());
+        return "security/register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(CustomUser customUser) {
+        CustomUserDAO dao = new CustomUserDAO(CustomUserSessionFactory.getCustomUserSessionFactory());
+        dao.saveUser(customUser);
+        return "redirect:/login";
+    }
+}
